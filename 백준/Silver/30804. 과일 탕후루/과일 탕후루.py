d929fe612651ec08dc785f72962b4ebbd2e6840b
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-05 21:45:07 +0900

    [Silver II] Title: 과일 탕후루, Time: 236 ms, Memory: 36804 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -5,12 +5,9 @@ fruit_list=list(map(int,input().split()))
 cnt=t_cnt=tmp=1
 chck_list=set()
 chck_list.add(fruit_list[0])
-q1=deque([fruit_list[0]])
-
 for i in range(1,len(fruit_list)):
     chck_list.add(fruit_list[i])
     if(len(chck_list)<=2):
-        q1.append(fruit_list[i])
         cnt+=1
         if(fruit_list[i]==fruit_list[i-1]):
             tmp+=1
@@ -18,12 +15,10 @@ for i in range(1,len(fruit_list)):
             tmp=1
         t_cnt=max(t_cnt,cnt)
     else:
-        chck_list=set([q1.pop()])
-        q1=deque(list(chck_list))
+        chck_list=set([fruit_list[i-1]])
         t_cnt=max(t_cnt,cnt)
         
         chck_list.add(fruit_list[i])
-        q1.append(fruit_list[i])
         cnt=tmp+1
         tmp=1
         
