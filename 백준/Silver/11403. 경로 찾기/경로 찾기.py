26743a4c74119f1d0b3414f7b4007905e1a5f3fc
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-07 11:14:22 +0900

    [Silver I] Title: 경로 찾기, Time: 132 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+n=int(sys.stdin.readline())
+adj_mat=[0 for _ in range(n)]
+for i in range(n):
+    adj_mat[i]=list(map(int,sys.stdin.readline().split()))
+
+def bfs(start):
+    global n,adj_mat,visited
+    
+    q=deque()
+    q.append(start)
+    parent=q.popleft()
+    for child,ox in enumerate(adj_mat[parent]):
+        if(ox==1 and visited[child]==0):
+            q.append(child)
+
+    while q:
+        parent=q.popleft()
+        if(visited[parent]==0):
+            visited[parent]=1
+            for child,ox in enumerate(adj_mat[parent]):
+                if(ox==1 and visited[child]==0):
+                    q.append(child)
+    adj_mat[start]=visited.copy()
+
+for i in range(n):
+    visited=[0]*n
+    bfs(i)
+    sys.stdout.write(" ".join(list(map(str,adj_mat[i]))))
+    sys.stdout.write(f'\n')
