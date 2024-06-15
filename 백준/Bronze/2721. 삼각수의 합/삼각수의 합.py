666f6eb9728c4f5c9fd0f51568745fd0b3675853
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-16 04:07:54 +0900

    [Bronze III] Title: 삼각수의 합, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2721. 삼각수의 합/삼각수의 합.py
+++ 백준/Bronze/2721. 삼각수의 합/삼각수의 합.py
@@ -1,4 +1,3 @@
-import sys
 t_list=[0]*302
 t_list[1]=1
 for i in range(2,302):
@@ -8,7 +7,6 @@ w_list[1]=1*t_list[2]
 for i in range(2,301):
     w_list[i]=w_list[i-1]+i*t_list[i+1]
 
-test_case=int(sys.stdin.readline())
+test_case=int(input())
 for _ in range(test_case):
-    t=int(sys.stdin.readline())
-    print(w_list[t])
+    print(w_list[int(input())])
