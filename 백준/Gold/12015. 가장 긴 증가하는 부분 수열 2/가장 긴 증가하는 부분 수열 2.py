e4184677a9e93f774391a4d3d409984219bffff2
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-16 03:57:39 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 984 ms, Memory: 157612 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -7,11 +7,11 @@ dp = [1]
 x = [array[0]]
 
 for i in range(1, len(array)):
-    if array[i] > x[-1]: # 현재 값이 x 배열의 마지막 값보다 클 경우
-        x.append(array[i]) # x 배열에 현재 값을 추가해 주고
-        dp.append(dp[-1] + 1) # 증가 부분 수열의 길이를 1 증가시킨다.
-    else: # 그렇지 않을 경우
-        idx = bisect_left(x, array[i]) # 현재 값이 x 배열의 몇 번째 인덱스에 들어갈 수 있는지를 찾아서
-        x[idx] = array[i] # x 배열의 idx 위치에 현재 값을 넣어준다.
+    if array[i] > x[-1]: 
+        x.append(array[i])
+        dp.append(dp[-1] + 1) 
+    else: 
+        idx = bisect_left(x, array[i])
+        x[idx] = array[i]
 
-print(dp[-1])
+sys.stdout.write(f'{dp[-1]}')
