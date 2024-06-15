96ab6acde513ffe4aa3b7748d8d186ce9031c403
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-16 03:30:31 +0900

    [Silver IV] Title: 큐, Time: 76 ms, Memory: 34072 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,35 @@
+
+from collections import deque
+import sys
+input=sys.stdin.readline
+print=sys.stdout.write
+
+n_test=int(input())
+deq=deque()
+for _ in range(n_test):
+    tmp_input = list(input().split())
+    if(len(tmp_input)>1):
+        tmp_input[1]=int(tmp_input[1])
+        if(tmp_input[0]=='push'):
+            deq.append(tmp_input[1])
+    else:
+        if (tmp_input[0]=='pop'):
+            try:
+                print(f'{deq.popleft()}\n')
+            except:
+                print(f'-1\n')
+        elif (tmp_input[0]=='size'):
+            print(f'{len(deq)}\n')
+        elif (tmp_input[0]=='empty'):
+            tmp= 1 if not deq else 0 
+            print(f'{tmp}\n')
+        elif (tmp_input[0]=='front'):
+            try:
+                print(f'{deq[0]}\n')
+            except:
+                print('-1\n')
+        elif (tmp_input[0]=='back'):
+            try:
+                print(f'{deq[-1]}\n')
+            except:
+                print('-1\n')            
