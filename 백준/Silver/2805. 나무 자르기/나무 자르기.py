d4fd2f1dcfb353e9d891cd72c0e66cec30083908
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-16 03:45:05 +0900

    [Silver II] Title: 나무 자르기, Time: 4664 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,26 +1,14 @@
-n,m = map(int, input().split())
-tree_list=list(map(int,input().split()))
-tree_list.sort(reverse=True) #내림차순
-sum_list=[0]*len(tree_list)
-sum_list[0]=tree_list[0]
-res=0
-idx=-1
-if(n==1):
-    print(tree_list[0]-m)
-else:
-    for i in range(1,len(tree_list)):
-        sum_list[i] = sum_list[i-1] + tree_list[i]
-        res = sum_list[i]-(tree_list[i]*(i+1))
-        if(res>=m):
-            idx=i
-            break
-    if (res==m):
-        #good
-        print(tree_list[idx])
-    elif (idx != -1):
-        #reverse traversal
-        x=int((sum_list[idx-1]-m)/idx)
-        print(x)
+N, M = map(int, input().split())
+tree = list(map(int, input().split()))
+start, end = 1, max(tree)
+while start <= end: 
+    mid = (start+end) // 2
+    log = 0 
+    for i in tree:
+        if i >= mid:
+            log += i - mid
+    if log >= M:
+        start = mid + 1
     else:
-        x=int((sum_list[len(tree_list)-1]-m)/len(tree_list))
-        print(x)
+        end = mid - 1
+print(end)
