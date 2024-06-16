04703870f47bb3caa60e563a4494481d25997444
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-16 19:43:22 +0900

    [Gold I] Title: 합, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1081. 합/합.py
+++ 백준/Gold/1081. 합/합.py
@@ -1,44 +1,23 @@
 import sys
-n, m = map(int, sys.stdin.readline().split())
-tmp=[0]*1000
-n_arr = [0 for _ in range(10)]
-m_arr = [0 for _ in range(10)]
-n -= 1
-score = 1
-while n != 0:
-    while n % 10 != 9:
-        for i in str(n):
-            n_arr[int(i)] += score
-        n -= 1
-    if n < 10:
-        for i in range(n + 1):
-            n_arr[i] += score
-        n_arr[0] -= score
-        break
-    else:
-        for i in range(10):
-            n_arr[i] += (n // 10 + 1) * score
-    n_arr[0] -= score
-    score *= 10
-    n //= 10
-score = 1
-while m != 0:
-    while m % 10 != 9:
-        for i in str(m):
-            m_arr[int(i)] += score
-        m -= 1
-    if m < 10:
-        for i in range(m + 1):
-            m_arr[i] += score
-        m_arr[0] -= score
-        break
-    else:
-        for i in range(10):
-            m_arr[i] += (m // 10 + 1) * score
-    m_arr[0] -= score
-    score *= 10
-    m //= 10
-ans = 0
-for i in range(10):
-    ans += (m_arr[i] - n_arr[i]) * i
-sys.stdout.write(f'{ans}')
+def f(n, i):
+    x = 0
+    j = 1
+    t = 0
+    while j <= n:
+        k = (n // j) % 10
+        if i < k:
+            x += j
+        elif i == k:
+            x += n % j + 1
+        x += k * t * (j // 10)
+        j *= 10
+        t += 1
+    return x
+L,U = map(int,sys.stdin.readline().split())
+s = 0
+for i in range(1, 10):
+    s += (f(U, i) - f(L, i)) * i
+while L:
+    s += L % 10
+    L //= 10
+sys.stdout.write(f'{s}')
