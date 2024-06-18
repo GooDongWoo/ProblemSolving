bab4e614d16ea45d077230b0ccbe8f8d5a1c5d80
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-19 06:08:54 +0900

    [Bronze III] Title: 연길이의 이상형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.py
@@ -0,0 +1,20 @@
+a=list(input().rstrip())
+if(a[0]=='I'):
+    print('E',end='')
+else:
+    print('I',end='')
+    
+if(a[1]=='N'):
+    print('S',end='')
+else:
+    print('N',end='')   
+     
+if(a[2]=='T'):
+    print('F',end='')
+else:
+    print('T',end='')   
+     
+if(a[3]=='J'):
+    print('P',end='')
+else:
+    print('J',end='')    
