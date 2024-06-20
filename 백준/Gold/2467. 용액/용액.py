fa19118d726839bb31c3d0b799e6862badf55dd0
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-20 15:53:39 +0900

    [Gold V] Title: 용액, Time: 104 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,31 +1,16 @@
 import sys
-from bisect import bisect_left as bisec
 inf=int(1e10)
 n=int(sys.stdin.readline())
 il=list(map(int,sys.stdin.readline().split()))
-zdx=bisec(il,0)
 res=inf
 start=0
 end=n-1
 t_s,t_e=-1,-1
-if(zdx+1<n):
-    sum_my=il[zdx]+il[zdx+1]
-    if(abs(sum_my)<res):
-        res=abs(sum_my)
-        t_s=zdx
-        t_e=zdx+1
-
-if(zdx-2>=0):
-    sum_my=il[zdx-2]+il[zdx-1]
-    if(abs(sum_my)<res):
-        res=abs(sum_my)
-        t_s=zdx-2
-        t_e=zdx-1
-
-while start<end and start<zdx and end>=zdx:
+while start<end:
     sum_my=il[start]+il[end]
-    if(abs(sum_my)<res):
-        res=abs(sum_my)
+    abs_sum=abs(sum_my)
+    if(abs_sum<res):
+        res=abs_sum
         t_s=start
         t_e=end
     if(sum_my>0):
