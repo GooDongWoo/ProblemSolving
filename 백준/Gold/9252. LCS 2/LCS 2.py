768a752158d5268b4cf2699c4be80c262997531b
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-24 23:57:59 +0900

    [Gold IV] Title: LCS 2, Time: 736 ms, Memory: 56596 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,25 +1,18 @@
 import sys
 a=list(sys.stdin.readline().rstrip())
 b=list(sys.stdin.readline().rstrip())
-dp_mat=[['' for _ in range(len(a)+1)] for _ in range(len(b)+1)]
+dp_mat=[[0 for _ in range(len(a)+1)] for _ in range(len(b)+1)]
 for i in range(1,len(b)+1):
     for j in range(1,len(a)+1):
-        if(a[j-1]==b[i-1]):
-            tmp=max(len(dp_mat[i-1][j-1]+a[j-1]), len(dp_mat[i][j-1]), len(dp_mat[i-1][j]))
-            if tmp==len(dp_mat[i-1][j-1]+a[j-1]):
-                dp_mat[i][j]=dp_mat[i-1][j-1]+a[j-1]
-                
-            elif tmp==len(dp_mat[i][j-1]):
-                dp_mat[i][j]=dp_mat[i][j-1]
-            else:
-                dp_mat[i][j]=dp_mat[i-1][j]
-        else:
-            tmp=max(len(dp_mat[i][j-1]), len(dp_mat[i-1][j]))
-            if tmp==len(dp_mat[i][j-1]):
-                dp_mat[i][j]=dp_mat[i][j-1]
-            else:
-                dp_mat[i][j]=dp_mat[i-1][j]            
-
+        tmp=(b[i-1]==a[j-1])
+        dp_mat[i][j]=max(dp_mat[i-1][j-1]+tmp, dp_mat[i][j-1], dp_mat[i-1][j])
 i=len(b)
 j=len(a)
-sys.stdout.write(f'{len(dp_mat[i][j])}\n{dp_mat[i][j]}')
+sys.stdout.write(f'{dp_mat[i][j]}\n')
+if(dp_mat[i][j]):
+    res=[]
+    while i>=1 and j>=1:
+        if((b[i-1]==a[j-1])):res.append(b[i-1]);i-=1;j-=1
+        elif(dp_mat[i][j]==dp_mat[i][j-1]):j-=1
+        else:i-=1
+    sys.stdout.write(f'{"".join(res[::-1])}\n')
