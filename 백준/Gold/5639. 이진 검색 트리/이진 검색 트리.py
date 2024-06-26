9711a56b8904d1e8af140dfb2f9bedb977ad6ec3
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-26 20:00:32 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2016 ms, Memory: 33628 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -24,3 +24,71 @@ while True:
     except:
         printPostorder(0,len(key_list)-1)
         break
+
+#객체로 구현하니 시간 초과 나옴... 그래서 그냥 배열로 풀었다..
+'''
+import sys
+sys.setrecursionlimit(int(1e9))
+class Node():
+    def __init__(self,value) -> None:
+        self.key=value
+        self.left=None
+        self.right=None
+
+root=Node(None)
+def addNode(key,parent=root):
+    if not (parent.key):#init val
+        parent.key=key
+    else:#normal add process
+        if(key<parent.key):#go left
+            if not(parent.left):
+                parent.left=Node(key)
+            else:
+                addNode(key,parent.left)
+        else:
+            if not(parent.right):
+                parent.right=Node(key)
+            else:
+                addNode(key,parent.right)
+
+def postorder(root):
+    if(root.left):
+        postorder(root.left)
+    if(root.right):
+        postorder(root.right)
+    sys.stdout.write(f'{root.key}\n')
+
+while True:
+    try:
+        key= int(sys.stdin.readline())
+        addNode(key)
+    except:
+        postorder(root)
+        break
+'''
+
+'''tree=[0]*10001
+def makeTree(key,parent=1):
+    global tree
+    if not (tree[1]):
+        tree[1]=key
+    else:#normal add process
+            if(key<tree[parent]):#go left
+                if not(tree[2*parent]):
+                    tree[2*parent]=key
+                else:
+                    makeTree(key,2*parent)
+            else:
+                if not(tree[2*parent +1]):
+                    tree[2*parent +1]=key
+                else:
+                    makeTree(key,2*parent +1)
+
+
+def postorder(root):
+    if(root.left):
+        postorder(root.left)
+    if(root.right):
+        postorder(root.right)
+    sys.stdout.write(f'{root.key}\n')
+'''
