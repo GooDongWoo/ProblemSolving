5464770b338f809ec613bb3dc2759a1b588e7daa
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-06-26 16:18:31 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.py
@@ -0,0 +1,16 @@
+n=int(input())
+whelk='@'
+for i in range(1,n+1):
+    print(whelk*5*n)
+    
+for i in range(1,n+1):
+    print(whelk*n)
+    
+for i in range(1,n+1):
+    print(whelk*5*n)
+    
+for i in range(1,n+1):
+    print(whelk*n)
+    
+for i in range(1,n+1):
+    print(whelk*5*n)
