f46803ff106c0b0bc7f8651495311177670e3ac2
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-07-04 19:23:15 +0900

    [Gold IV] Title: LCS 3, Time: 336 ms, Memory: 38640 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.py
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -1,4 +1,27 @@
 import sys
+input = sys.stdin.readline
+
+def get_LCS(w1, w2, w3):
+    dp = [[[0]*len(w3) for _ in range(len(w2))] for _ in range(len(w1))]
+    
+    for i in range(1,len(w1)):
+        for j in range(1,len(w2)):
+            for k in range(1,len(w3)):
+                if w1[i] == w2[j] == w3[k]:
+                    dp[i][j][k] = dp[i-1][j-1][k-1]+1
+                else:
+                    dp[i][j][k] = max(dp[i-1][j][k],dp[i][j-1][k],dp[i][j][k-1])
+        
+    return dp[-1][-1][-1]
+    
+
+if __name__ == '__main__':
+    w1 = ' '+input().strip()
+    w2 = ' '+input().strip()
+    w3 = ' '+input().strip()
+    
+    print(get_LCS(w1,w2,w3))
+'''import sys
 a=(sys.stdin.readline().rstrip())
 b=(sys.stdin.readline().rstrip())
 c=(sys.stdin.readline().rstrip())
@@ -10,4 +33,4 @@ for k in range(1,len(c)+1):
                 dp[k][i][j]=dp[k-1][i-1][j-1]+1
             else:
                 dp[k][i][j]=max(dp[k][i][j-1],dp[k][i-1][j],dp[k-1][i][j])
-sys.stdout.write(f'{dp[-1][-1][-1]}')
+sys.stdout.write(f'{dp[-1][-1][-1]}')'''
