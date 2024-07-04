48586ccad2cd39c3a41871eb81ca632b9b1d1a04
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-07-04 19:20:27 +0900

    [Gold IV] Title: LCS 3, Time: 940 ms, Memory: 38640 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.py
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -6,5 +6,5 @@ dp=[[[0]*(len(a)+1) for _ in range(len(b)+1)]for _ in range(len(c)+1)]
 for k in range(1,len(c)+1):
     for i in range(1,len(b)+1):
         for j in range(1,len(a)+1):
-                dp[k][i][j]=max(dp[k][i-1][j-1],dp[k-1][i][j-1],dp[k-1][i-1][j],dp[k][i][j-1],dp[k][i-1][j],dp[k-1][i][j],dp[k-1][i-1][j-1]+(a[j-1]==b[i-1] and b[i-1]==c[k-1]))
+                dp[k][i][j]=max(dp[k][i][j-1],dp[k][i-1][j],dp[k-1][i][j],dp[k-1][i-1][j-1]+(a[j-1]==b[i-1]==c[k-1]))
 sys.stdout.write(f'{dp[-1][-1][-1]}')
