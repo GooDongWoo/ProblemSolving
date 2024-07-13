7ba7cab7bff274be5f6cd089b960661214ed075d
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-07-13 19:16:04 +0900

    [Gold V] Title: 빗물, Time: 100 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -2,13 +2,12 @@ import sys
 h,w=map(int,sys.stdin.readline().split())
 walls=[*map(int,sys.stdin.readline().split())]
 start=0
-end=len(walls)-1
 res=0
 def sol(start):
-    global h,w,walls,res,end
+    global h,w,walls,res
     init=start
     idx=start+1
-    while idx<=end:
+    while idx<=len(walls)-1:
         if(walls[idx]>=walls[start]):#지금까지의 합을 계산하고 이를 더하고 새로운 start만들고 idx=start+1
             th=min(walls[start],walls[idx])
             for i in range(start,idx+1):
@@ -18,8 +17,8 @@ def sol(start):
         else:
             idx+=1
     if(init==start):
-        walls[start]=max(walls[init+1:end+1])
+        walls[start]=max(walls[init+1:len(walls)])
     return start
-while start<end:
+while start<len(walls)-1:
     start=sol(start)
 print(res)
