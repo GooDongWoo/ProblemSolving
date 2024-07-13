24be395155214fff0f50ea47c5034b83a40a7085
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-07-13 18:45:14 +0900

    [Gold IV] Title: Lietus, Time: 524 ms, Memory: 54728 KB -BaekjoonHub

---
+++ 백준/Gold/30344. Lietus/Lietus.py
@@ -0,0 +1,30 @@
+import sys
+w,h=map(int,sys.stdin.readline().split())
+walls=[int(sys.stdin.readline()) for _ in range(w)]
+start=0
+end=len(walls)-1
+while not walls[start] or not walls[end]:
+    if(not walls[start]):
+        start+=1
+    elif(not walls[end]):
+        end-=1
+res=0
+def sol(start):
+    global h,w,walls,res,end
+    init=start
+    idx=start+1
+    while idx<=end:
+        if(walls[idx]>=walls[start]):#지금까지의 합을 계산하고 이를 더하고 새로운 start만들고 idx=start+1
+            th=min(walls[start],walls[idx])
+            for i in range(start,idx+1):
+                res+=max(th-walls[i],0)
+            start=idx
+            idx+=1
+        else:
+            idx+=1
+    if(init==start):
+        walls[start]=max(walls[init+1:end+1])
+    return start
+while start<end:
+    start=sol(start)
+print(res)
