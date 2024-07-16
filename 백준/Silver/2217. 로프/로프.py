5010436063ecca063ad13e126e1036f3f19ea0ee
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-07-16 16:32:50 +0900

    [Silver IV] Title: 로프, Time: 96 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,7 +1,9 @@
-n=int(input())
-ropes=[int(input()) for _ in range(n)]
+import sys
+n=int(sys.stdin.readline())
+ropes=[int(sys.stdin.readline()) for _ in range(n)]
 ropes.sort()
 c=[0]*n
-for i in range(n):
+c[0]=(n)*(ropes[0])
+for i in range(1,n):
     c[i]=(n-i)*(ropes[i])
 print(max(c))
