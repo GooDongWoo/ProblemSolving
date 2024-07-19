beaba45b7476f49216af2e88bd2cc255f98380ec
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-07-19 18:33:32 +0900

    [Silver III] Title: 카드 놓기, Time: 752 ms, Memory: 164948 KB -BaekjoonHub

--- 백준/Silver/18115. 카드 놓기/카드 놓기.py
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.py
@@ -5,15 +5,13 @@ methods=[*map(int,sys.stdin.readline().split())]
 cards=deque()
 for i in range(n):
     if(methods[n-1-i]==1):#제일 위에 올려 놓음
-        cards.append(i)
+        cards.append(i+1)
         
     elif(methods[n-1-i]==2):#제일 위 팝하고 그위에다가 올리고 팝한 것을 다시 쌓음(2번째에 쌓는다는 말)
         tmp=cards.pop()
-        cards.append(i)
+        cards.append(i+1)
         cards.append(tmp)
         
     else:#제일 아래에다가 넣는다.
-        cards.appendleft(i)
-        
-for _ in range(n):
-    sys.stdout.write(f'{cards.pop()+1} ')
+        cards.appendleft(i+1)
+sys.stdout.write(f'{" ".join([*map(str,reversed(list(cards)))])}')
