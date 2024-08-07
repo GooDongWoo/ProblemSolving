e7f9495554dd8e1cf4ccbb2f3fdd20af4d2282dd
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-08-07 13:20:09 +0900

    [Bronze III] Title: Disposable Cup, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/13129. Disposable Cup/Disposable Cup.py
+++ 백준/Bronze/13129. Disposable Cup/Disposable Cup.py
@@ -1,10 +1,3 @@
 a,b,n=map(int,input().split())
-up=set([a+b])
-down=set([a+b])
-for i in range(1,n):
-    new_up=set([d+(a+b) for d in down])|set([u+(a) for u in up])
-    new_down=set([d+(a) for d in down])|set([u+(a+b) for u in up])
-    up=new_up
-    down=new_down
-result=sorted(list(up|down))
-print(*result)
+l1=[(a+b)*(n-i)+a*(i) for i in range(n-1,-1,-1)]
+print(*l1)
