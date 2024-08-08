17ec0894ff8d6be30d8c1f9a98d9a4c91a351b7d
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-08-08 17:52:57 +0900

    [Silver III] Title: 콘센트, Time: 136 ms, Memory: 45888 KB -BaekjoonHub

---
+++ 백준/Silver/7774. 콘센트/콘센트.py
@@ -0,0 +1,33 @@
+n,m=map(int,input().split())
+a=[]
+b=[]
+if(n):
+    a=sorted([*map(int,input().split())],reverse=True)
+if(m):
+    b=sorted([*map(int,input().split())],reverse=True)
+
+def sol():
+    global n,m,a,b
+    if(n==0):
+        print(1)
+        return
+    if(m==0):
+        print(1)
+        return
+    adx=0
+    bdx=0
+    flag=1
+    ca=1
+    cb=0
+    while flag:
+        ca-=1
+        cb+=a[adx]
+        adx+=1
+        while cb!=0 and bdx<len(b):
+            cb-=1
+            ca+=b[bdx]
+            bdx+=1
+        flag=(ca>0) and (adx<len(a)) and (bdx<len(b))
+    print(ca)
+
+sol()
