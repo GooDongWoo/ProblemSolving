8c7df1996feef15f036971e5e20274d5443851ec
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-08-11 04:57:55 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 616 ms, Memory: 67012 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,40 +1,29 @@
 from collections import deque
 import sys
-input= sys.stdin.readline
-print=sys.stdout.write
-
-def dfs(n, adj_m, chck_bef):
-    tmp=deque()
-    chck_bef[n]=1
-    for i in range(len(adj_m[n])):
-        if chck_bef[adj_m[n][i]]==0:
-            tmp.append(adj_m[n][i])
-            chck_bef[adj_m[n][i]]=1
-        else:
-            pass
-    if tmp:
-        dfs(tmp.pop(), adj_m, chck_bef)
-    else:
-        return
-
-#input
-n_node, n_edge=map(int,input().split())
-
-#initialize
-chck_bef=[0]*(n_node+1)
-adj_m=[[] for _ in range(n_node+1)]
-cnt=0
-
-#edge input
-for _ in range(n_edge):
-    n1, n2=map(int,input().split())
+n, m=map(int,sys.stdin.readline().split())
+adj_m=[[] for _ in range(n+1)]
+v=[0]*(n+1)
+for _ in range(m):
+    n1, n2=map(int,sys.stdin.readline().split())
     adj_m[n1].append(n2)
     adj_m[n2].append(n1)
 
-#using deque DFS 
-for i in range(1,n_node+1):
-    if chck_bef[i]==0:
+def dfs(start):
+    global n,m,adj_m,v
+    q=deque()
+    q.append(start)
+    while q:
+        p=q.pop()
+        if(v[p]==0):
+            v[p]=1
+            for c in adj_m[p]:
+                if(v[c]==0):
+                    q.append(c)
+    
+cnt=0
+for i in range(1,n+1):
+    if v[i]==0:
         cnt+=1
-        dfs(i,adj_m, chck_bef)
+        dfs(i)
 
-print(f'{cnt}')
+sys.stdout.write(f'{cnt}')
