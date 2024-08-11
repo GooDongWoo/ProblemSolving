1e7f1999f5b9dbad3ac9aa87ea76c23a5e111c9e
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-08-11 19:24:08 +0900

    [Silver I] Title: 여섯 방정식, Time: 32 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/9765. 여섯 방정식/여섯 방정식.py
+++ 백준/Silver/9765. 여섯 방정식/여섯 방정식.py
@@ -1,43 +1,9 @@
+import math
 c1,c2,c3,c4,c5,c6=map(int,input().split())
-primes=[]
-candidates=[1]*20000001
-candidates[0]=candidates[1]=0
-for i in range(2,20000001):
-    if(candidates[i]==1):
-        primes.append(i)
-        multi=2
-        while multi*i<20000001:
-            candidates[multi*i]=0
-            multi+=1
-x1c=[]
-for i in range(len(primes)):
-    if(c1%primes[i]==0):
-        x1c.append(primes[i])
-        break
-x1c.append(c1//x1c[0])
-if ((c5%x1c[0])==0):
-    x1=x1c[1]
-    x2=x1c[0]
-    x3=c5//x1c[0]
-else:
-    x1=x1c[0]
-    x2=x1c[1]
-    x3=c5//x1c[1]
-
-#################
-x5c=[]
-for i in range(len(primes)):
-    if(c6%primes[i]==0):
-        x5c.append(primes[i])
-        break
-x5c.append(c6//x5c[0])
-if ((c3%x5c[0])==0):
-    x5=x5c[1]
-    x6=x5c[0]
-    x7=c3//x5c[0]
-else:
-    x5=x5c[0]
-    x6=x5c[1]
-    x7=c3//x5c[1]
-
+x2=math.gcd(c1,c5)
+x1=c1//x2
+x3=c5//x2
+x6=math.gcd(c6,c3)
+x5=c6//x6
+x7=c3//x6
 print(x1, x2, x3, x5, x6, x7)
