9fb6f3363014fe6ecbc333af896c7e606be7cbde
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-08-11 17:57:52 +0900

    [Silver II] Title: 화분 부수기, Time: 688 ms, Memory: 108036 KB -BaekjoonHub

--- 백준/Silver/3088. 화분 부수기/화분 부수기.py
+++ 백준/Silver/3088. 화분 부수기/화분 부수기.py
@@ -4,8 +4,7 @@ pots=[list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 cnt=0
 target_num=[0]*1000001
 for i in range(n):
-    chck=any([(target_num[j]==1) for j in pots[i]])
-    if not (chck):
+    if not (target_num[pots[i][0]] or target_num[pots[i][1]] or target_num[pots[i][2]]):
         cnt+=1
     for j in pots[i]:
         target_num[j]=1
