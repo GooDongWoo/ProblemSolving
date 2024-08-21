7a38afe52dbd9ccc1a62adae7b7b02b7cfdc6915
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-08-21 13:47:02 +0900

    [Silver II] Title: 222-풀링, Time: 616 ms, Memory: 75048 KB -BaekjoonHub

---
+++ 백준/Silver/17829. 222－풀링/222－풀링.py
@@ -0,0 +1,13 @@
+n=int(input())
+mat=[list(map(int,input().split())) for _ in range(n)]
+dy_dx=[(0,0),(1,0),(0,1),(1,1)]
+while n//2:
+    n=n//2
+    nmat=[[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            tmp=sorted([mat[2*i+dy][2*j+dx] for dy,dx in dy_dx])
+            nmat[i][j]=tmp[-2]
+    mat=nmat
+print(nmat[-1][-1])
+    
