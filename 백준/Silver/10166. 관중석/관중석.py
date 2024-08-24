41805cad5e80229eee504b6ba7a10d507d19291a
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-08-24 18:10:03 +0900

    [Silver I] Title: 관중석, Time: 88 ms, Memory: 34708 KB, Score: 62 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.py
+++ 백준/Silver/10166. 관중석/관중석.py
@@ -1,10 +1,17 @@
-import sys
-d1,d2=map(int,sys.stdin.readline().split())
+def  gcd(a1,a2):
+    b=max(a1,a2)
+    s=min(a1,a2)
+    if(s==0):return -1
+    if(b%s==0):return s
+    return gcd(s,b%s)
+
+d1,d2=map(int,input().split())
 occupied=set()
 for nth in range(d1,d2+1):
-    step=360/nth
-    mul=0
-    while step*mul<360:
-        occupied.add(step*mul)
-        mul+=1
+    for j in range(nth):
+        res=gcd(j,nth)
+        if(res==-1):
+            occupied.add((0,360))
+        else:
+            occupied.add((j//res,nth//res))
 print(len(occupied))
