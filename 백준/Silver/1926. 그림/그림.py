2edf1db9b94b2fdc22a795e04a5ac9e3e98c9706
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-08-27 10:03:59 +0900

    [Silver I] Title: 그림, Time: 336 ms, Memory: 36608 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -7,6 +7,7 @@ dys=[0,0,-1,1]
 v=[[0]*m for _ in range(n)]
 candidates=[0]
 def bfs(sy,sx):
+    global v,n,m,mat,dys,dxs
     q=deque()
     q.append((sy,sx))
     v[sy][sx]=1
@@ -19,9 +20,7 @@ def bfs(sy,sx):
             if (0<=ny<n and 0<=nx<m) and (mat[ny][nx]==1 and v[ny][nx]==0):
                 v[ny][nx]=1
                 q.append((ny,nx))
-    
     return cnt
-
 for i in range(n):
     for j in range(m):
         if mat[i][j]==1 and v[i][j]==0:
