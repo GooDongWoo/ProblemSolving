aeeebd03ba1f847e028f80a2c9adbe8f23191eda
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-09-01 15:12:59 +0900

    [Silver I] Title: 월요일-토요일, Time: 5996 ms, Memory: 34792 KB -BaekjoonHub

---
+++ 백준/Silver/4969. 월요일－토요일/월요일－토요일.py
@@ -0,0 +1,24 @@
+import sys
+L=300000
+ms_num=[0]*(L+1)
+for i in range(L+1):
+    if(i%7==1 or i%7==6):ms_num[i]=1
+ms_prime_list=[]
+for i in range(2,L+1):
+    if(ms_num[i]==1):
+        ms_prime_list.append(i)
+        for j in range(i*2,L+1,i):
+            ms_num[j]=0
+
+while 1:
+    t=int(sys.stdin.readline())
+    if(t==1):break
+    sys.stdout.write(f'{t}: ')
+    res=[]
+    idx=0
+    while idx<len(ms_prime_list) and ms_prime_list[idx]<=t:
+        if(t%ms_prime_list[idx]==0):
+            res.append(ms_prime_list[idx])
+            sys.stdout.write(f'{ms_prime_list[idx]} ')
+        idx+=1
+    sys.stdout.write(f'\n')
