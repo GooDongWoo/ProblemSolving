9e34ca5bb64291eb95ea6a2e8f1bd35cb31fd9c2
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-09-16 17:46:44 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 2224 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,42 @@
+from collections import deque
+n,k=map(int,input().split())
+a=deque(map(int,input().split()))
+robots=deque([0]*(2*n))
+zero_cnt=0
+
+def rotate():
+    global robots,a
+    robots[n-1]=0
+    robots.rotate(1)
+    robots[n-1]=0
+    a.rotate(1)
+
+def move():
+    global a,robots
+    for idx in range(n-2,0,-1):
+        if(robots[idx]!=0 and robots[idx+1]==0 and a[idx+1]>0):
+            robots[idx]=0
+            robots[idx+1]=1
+            a[idx+1]-=1
+
+def load():
+    if(a[0]!=0):
+        robots[0]=1
+        a[0]-=1
+
+def zeroCnting():
+    tmp=0
+    for item in a:
+        if(item==0): tmp+=1
+    
+    return tmp
+
+level=0
+while zero_cnt<k:
+    level+=1
+    rotate()
+    move()
+    load()
+    zero_cnt=zeroCnting()
+
+print(level)
