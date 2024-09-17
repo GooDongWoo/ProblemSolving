76da8c9e911caa763e638c57b2850bec7486f282
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-09-17 15:39:05 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,19 @@
+n,m=map(int,input().split())
+l1=sorted(list(map(int,input().split())))
+def makecases():
+    cases=[]
+    tmp=[0]*m
+    def bt(idx,bef):
+        if(idx==m):
+            real1=[l1[i] for i in tmp]
+            cases.append(real1.copy())
+        else:
+            for i in range(bef+1,n):
+                tmp[idx]=i
+                bt(idx+1,i)
+    
+    bt(0,-1)
+    return cases
+
+for i in makecases():
+    print(*i)
