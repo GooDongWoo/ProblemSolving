ee436abf607f144f3ad9ef59749b937b88a1331c
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-10-02 18:29:12 +0900

    [Silver III] Title: 프린터 큐, Time: 52 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,31 +1,23 @@
 from collections import deque
-import heapq
-n_test=int(input())
-deq=deque()
-
-for _ in range(n_test):
-    num_paper, idx_target=map(int,input().split())
-    priority_list=list(map(int,input().split()))
-    #heap1=priority_list.copy() #최소힙이라 안됨 큰 순서대로 나가야하는데;;
-    heap1=[0]*len(priority_list)
-    for i in range(len(priority_list)):
-        heap1[i]=-priority_list[i]
-    heapq.heapify(heap1)
-    deq=deque(priority_list)
+import sys
+input=sys.stdin.readline
+t=int(input())
+for _ in range(t):
+    n,m=map(int,input().split())
+    ps=list(map(int,input().split()))
+    ps=[(ps[i],i) for i in range(n)]
+    q=deque(ps)
+    
+    ps=sorted(ps,reverse=True)
+    pdx=0
     cnt=0
-    #code
-    while(deq):
-        if(deq[0]==-heap1[0]):
-            deq.popleft()
-            heapq.heappop(heap1)
+    while q:
+        p,idx=q.popleft()
+        if(ps[pdx][0]==p):
+            pdx+=1
             cnt+=1
-            if(idx_target==0):
+            if(idx==m):
                 print(cnt)
                 break
-            idx_target-=1
         else:
-            deq.append(deq.popleft())
-            if(idx_target==0):
-                idx_target=len(deq)-1
-            else:
-                idx_target-=1
+            q.append((p,idx))
