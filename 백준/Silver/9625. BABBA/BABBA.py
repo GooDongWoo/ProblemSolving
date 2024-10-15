383cac3ad15eb1066d5fa81c02b2d889541eb730
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-10-15 18:29:36 +0900

    [Silver V] Title: BABBA, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n=int(input())
+dp=[[0]*2 for _ in range(n+1)]
+dp[0][0]=1
+for i in range(1,n+1):
+    dp[i][0]=dp[i-1][1]
+    dp[i][1]=dp[i-1][0]+dp[i-1][1]
+print(*dp[n])
