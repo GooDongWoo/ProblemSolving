eebfac04e2179d556d451bcd0463034853e1f262
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-10-27 21:47:50 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 2032 ms, Memory: 316972 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -0,0 +1,35 @@
+import sys
+import math
+input=sys.stdin.readline
+
+def insert(y,x):
+    global base,tmp
+    by=len(base)
+    bx=len(base[0])
+    for i in range(by):
+        for j in range(bx):
+            tmp[i+y][j+x]=base[i][j]
+    
+    return
+
+n=int(input())
+k=int(math.log2(n//3))
+base=[[" "," ","*"," "," "],
+[" ","*"," ","*"," "],
+["*","*","*","*","*"]]
+y=3;x=5
+for i in range(1,k+1):
+    ny=y*2
+    nx=x*2+1
+    tmp=[[' ']*nx for _ in range(ny)]
+    by=len(base)
+    bx=len(base[0])
+    insert(by,0)
+    insert(by,bx+1)
+    insert(0,((x-1)//2)+1)
+    
+    y,x=(ny,nx)
+    base=tmp
+
+for line in base:
+    print(*line,sep='')
