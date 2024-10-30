9a1b5f240e6e1dd0d9700b89274acba80a411056
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-10-30 17:04:19 +0900

    [Silver II] Title: 토너먼트 승자, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1404. 토너먼트 승자/토너먼트 승자.py
+++ 백준/Silver/1404. 토너먼트 승자/토너먼트 승자.py
@@ -28,40 +28,23 @@ bt(0)
 ans=[0]*n
 for sim in res:
     prob=1
-    if sim[0]:
-        l1=0;prob*=adj_m[0][1]
-    else:
-        l1=1;prob*=adj_m[1][0]
-        
-    if sim[1]:
-        l2=2;prob*=adj_m[2][3]
-    else:
-        l2=3;prob*=adj_m[3][2]
-        
-    if sim[2]:
-        r1=4;prob*=adj_m[4][5]
-    else:
-        r1=5;prob*=adj_m[5][4]
+    if sim[0]:l1=0;prob*=adj_m[0][1]
+    else:l1=1;prob*=adj_m[1][0]
+    if sim[1]:l2=2;prob*=adj_m[2][3]
+    else:l2=3;prob*=adj_m[3][2]
+    if sim[2]:r1=4;prob*=adj_m[4][5]
+    else:r1=5;prob*=adj_m[5][4]
     
-    if sim[3]:
-        r2=6;prob*=adj_m[6][7]
-    else:
-        r2=7;prob*=adj_m[7][6]
-        
-    if sim[4]:
-        f1=l1;prob*=adj_m[l1][l2]
-    else:
-        f1=l2;prob*=adj_m[l2][l1]
+    if sim[3]:r2=6;prob*=adj_m[6][7]
+    else:r2=7;prob*=adj_m[7][6]
+    if sim[4]:f1=l1;prob*=adj_m[l1][l2]
+    else:f1=l2;prob*=adj_m[l2][l1]
     
-    if sim[5]:
-        f2=r1;prob*=adj_m[r1][r2]
-    else:
-        f2=r2;prob*=adj_m[r2][r1]
+    if sim[5]:f2=r1;prob*=adj_m[r1][r2]
+    else:f2=r2;prob*=adj_m[r2][r1]
     
-    if sim[6]:
-        winner=f1;prob*=adj_m[f1][f2]
-    else:
-        winner=f2;prob*=adj_m[f2][f1]
+    if sim[6]:winner=f1;prob*=adj_m[f1][f2]
+    else:winner=f2;prob*=adj_m[f2][f1]
     ans[winner]+=prob
 
 print(*ans)
