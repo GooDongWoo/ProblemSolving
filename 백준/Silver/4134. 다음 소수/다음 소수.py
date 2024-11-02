ef8bf942598c3453ea116d0cc7de00ef10d5f21f
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-11-02 14:40:43 +0900

    [Silver IV] Title: 다음 소수, Time: 1112 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -0,0 +1,28 @@
+import sys
+input=sys.stdin.readline
+MAX=(int(int(1e10)**(0.5))+1)
+primes=[True]*MAX
+primes[0]=primes[1]=False
+for i in range(MAX):
+    if(primes[i]):
+        for j in range(i**2,MAX,i):
+            primes[j]=False
+
+prime_list=[i for i in range(MAX) if primes[i]]
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    if(n==1 or n==0):
+        print(2)
+        continue
+    while 1:
+        failFlag=False
+        for i in prime_list:
+            if(i>=(int(n**(0.5))+1)):break
+            if(n%i==0):
+                failFlag=True
+                break
+        if not (failFlag):
+            print(n)
+            break
+        n+=1
