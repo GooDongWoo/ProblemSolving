67c843993e7a726b6ec2343b4fb0f663056603c1
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-11-11 12:34:36 +0900

    [Gold V] Title: 전깃줄, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -0,0 +1,26 @@
+import sys
+input=sys.stdin.readline
+n = int(input())
+ll=sorted([list(map(int, input().split())) for _ in range(n)])
+dp=[0]*n
+dp[0]=1
+
+for i in range(1,n):
+    tmpLIS=[]
+    
+    for j in range(i):
+        if(ll[j][1]<ll[i][1]):
+            tmpLIS.append(ll[j][1])
+    tmpLIS.append(ll[i][1])
+    
+    minilen=len(tmpLIS)
+    minidp=[1]*minilen
+    
+    for j in range(1,minilen):
+        for k in range(j):
+            if(tmpLIS[j]>tmpLIS[k]):
+                minidp[j]=max(minidp[j],minidp[k]+1)
+
+    result=max(minidp) if minidp else 1
+    dp[i]=max(dp[i-1],result)
+print(n-dp[-1])
