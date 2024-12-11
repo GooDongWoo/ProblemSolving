e83394178b3cca62d706c130e71ea6964fdf93c1
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2024-12-11 13:38:15 +0900

    [Silver III] Title: 거북이, Time: 1564 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/8911. 거북이/거북이.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+dy_dx = [(1, 0),(0, 1),(-1, 0),(0, -1)] #하, 오, 위, 왼
+
+t = int(input())
+for _ in range(t):
+    drt = 2
+    sy = sx = 0
+    l1 = [(sy, sx)]
+    opers = input().rstrip()
+    for i in range(len(opers)):
+        oper = opers[i]
+        if oper == 'F':
+            sy += dy_dx[drt][0]
+            sx += dy_dx[drt][1]
+            l1.append((sy,sx))
+        elif oper == 'B':
+            sy += dy_dx[(drt + 2) % 4][0]
+            sx += dy_dx[(drt + 2) % 4][1]
+            l1.append((sy,sx))
+        elif oper == 'L':
+            drt = (drt + 1) % 4
+        elif oper == 'R':
+            drt = (drt - 1) % 4
+    
+    miny = minx = maxy = maxx = 0
+    for i in range(len(l1)):
+        miny = min(miny, l1[i][0])
+        maxy = max(maxy, l1[i][0])
+        
+        minx = min(minx, l1[i][1])
+        maxx = max(maxx, l1[i][1])
+    
+    print((maxx - minx) * (maxy - miny))
