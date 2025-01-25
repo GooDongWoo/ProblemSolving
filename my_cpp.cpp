50b3e4e24439406da9ab2d2752125edc54f1f958
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-01-26 01:19:00 +0900

    Create my_cpp.cpp
    my cpp file form added

---
+++ my_cpp.cpp
@@ -0,0 +1,19 @@
+/*cpp standard file form*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0)
+#define rep(i, a, b) for (int i=a;i<b;++i)
+#define fi first
+#define se second
+#define LL long long
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+int main() {
+    return 0;
+}
