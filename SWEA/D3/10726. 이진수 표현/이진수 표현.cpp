6f43a5641ed590b62a63525e88d63ebe43b241fd
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-02-17 15:31:57 +0900

    [D3] Title: 이진수 표현, Time: 447 ms, Memory: 5,832 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
@@ -1,28 +1,25 @@
 #include<iostream>
+#include <cstring>
+#include <algorithm>
 
+#define rep(i,a,b) for(int i=a;i<b;i++)
 using namespace std;
+
+int N, M;
+
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
 
-	cin>>T;
+	cin >> T;
 
-	for(test_case = 1; test_case <= T; ++test_case)
-	{
-        int n, m, target;
-        bool flag = 1;
-        cin >>n >> m;
-        int nm = m;
-        for(int i=0;i<n;i++){
-        	if((nm % 2) != 1) flag = 0;
-            nm /= 2;
-        }
-        if(flag)printf("#%d ON\n", test_case);
-        else printf("#%d OFF\n", test_case);
-
-
+	for (test_case = 1; test_case <= T; ++test_case) {
+		cin >> N >> M;
+		int target = (1 << N) - 1;
+		if ((M & target) == target) printf("#%d ON\n", test_case);
+		else printf("#%d OFF\n", test_case);
 	}
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+	return 0;
 }
