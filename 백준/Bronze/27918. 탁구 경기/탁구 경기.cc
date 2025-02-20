0090520a474e30df61c06a0242311701b2bdeebf
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-02-20 10:26:44 +0900

    [Bronze IV] Title: 탁구 경기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27918. 탁구 경기/탁구 경기.cc
+++ 백준/Bronze/27918. 탁구 경기/탁구 경기.cc
@@ -1,22 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0)
 #define rep(i, a, b) for (int i=a;i<b;++i)
-#define fi first
-#define se second
-#define LL long long
+
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <tuple>
-#include <queue>
-#include <stack>
-#include <deque>
-#include <map>
-#include <set>
-#include <numeric>
 #include <cmath>
-#include <cstring>
 
 using namespace std;
 
