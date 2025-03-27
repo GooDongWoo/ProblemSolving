fe18d74aa9f71118e5f28b3501087cca34fd6730
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-03-27 16:20:12 +0900

    [Gold I] Title: 파도반 수열 2, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/27435. 파도반 수열 2/파도반 수열 2.cc
+++ 백준/Gold/27435. 파도반 수열 2/파도반 수열 2.cc
@@ -1,69 +1,55 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #define rep(i, a, b) for(int i = a; i < b; ++i)
 #define LL long long
 using namespace std;
 const LL MOD = 998244353;
-
-LL arr[6] = { 0, 1, 1, 1, 2, 2 };
-
+int T;
+LL N, arr[5] = { 1,1,1,2,2 };
 vector<vector<LL>> bmat(5, vector<LL>(5, 0));
 
 vector<vector<LL>> matmul(const vector<vector<LL>>& m1, const vector<vector<LL>>& m2) {
-    vector<vector<LL>> res(5, vector<LL>(5, 0));
-    rep(i, 0, 5)
-        rep(j, 0, 5)
-        rep(k, 0, 5) {
-        res[i][j] = (res[i][j] + (m1[i][k] * m2[k][j]) % MOD) % MOD;
-    }
-    return res;
+	vector<vector<LL>> res(5, vector<LL>(5, 0));
+	rep(i, 0, 5)
+		rep(j, 0, 5)
+		rep(steps, 0, 5)
+		res[i][j] = (res[i][j] + m1[i][steps] * m2[steps][j]) % MOD;
+	return res;
 }
 
-vector<vector<LL>> matrix_power(LL num) {
-    if (num <= 1)
-        return bmat;
-
-    vector<vector<LL>> half = matrix_power(num / 2);
-    vector<vector<LL>> result = matmul(half, half);
-
-    if (num & 1) {
-        result = matmul(result, bmat);
-    }
-    return result;
+vector<vector<LL>> recursive(LL num) {
+	if (num <= 1)
+		return bmat;
+	vector<vector<LL>> tmp = recursive(num / 2);
+	tmp = matmul(tmp, tmp);
+	if (num & 1) {
+		return matmul(tmp, bmat);
+	}
+	return tmp;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    bmat = {
-        {1, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0},
-        {0, 1, 0, 0, 0},
-        {0, 0, 1, 0, 0},
-        {0, 0, 0, 1, 0}
-    };
-
-    int T;
-    cin >> T;
-
-    while (T--) {
-        LL N;
-        cin >> N;
-
-        if (N <= 5) {
-            cout << arr[N] << "\n";
-            continue;
-        }
-        vector<vector<LL>> result_matrix = matrix_power(N - 5);
-
-        LL ans = 0;
-        rep(i, 0, 5) {
-            ans = (ans + (result_matrix[0][i] * arr[5 - i]) % MOD) % MOD;
-        }
-
-        cout << ans << "\n";
-    }
-
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	bmat =
+	{ {1,0,0,0,1},
+	  {1,0,0,0,0},
+	  {0,1,0,0,0},
+	  {0,0,1,0,0},
+	  {0,0,0,1,0} };
+	cin >> T;
+	rep(t, 0, T) {
+		cin >> N;
+		if (N < 6) {
+			printf("%lld\n", arr[N - 1]);
+			continue;
+		}
+		vector<vector<LL>> tmp = recursive(N-1);
+		LL ans = 0;
+		rep(i, 0, 5)
+			ans = (ans + tmp[4][i] * arr[4 - i]) % MOD;
+		printf("%lld\n", ans);
+	}
+	return 0;
 }
