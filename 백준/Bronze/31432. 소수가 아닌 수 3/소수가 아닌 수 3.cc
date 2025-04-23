8fe08731045910109b036013eb4bd03293d3d9ca
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-04-23 09:23:32 +0900

    [Bronze I] Title: 소수가 아닌 수 3, Time: 0 ms, Memory: 2020 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Bronze/31432. 소수가 아닌 수 3/소수가 아닌 수 3.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+#include<climits>
+#include<vector>
+#include<unordered_map>
+#include<cstring>
+
+using namespace std;
+
+#define fastio ios_base ::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define rep(i,a,b) for(int i=a;i<b;i++)
+
+int N;
+
+int main() {
+	fastio;
+	int tt;
+	cin >> N;
+	rep(i, 0, N)
+		cin >> tt;
+	printf("YES\n");
+	if (tt)
+		printf("%d%d", tt, tt);
+	else
+		printf("%d", tt);
+}
