2234216280373d45089cda2b922896439f06e196
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-06-02 11:57:41 +0900

    [Gold V] Title: 잘못된 LIS 알고리즘, Time: 36 ms, Memory: 4368 KB -BaekjoonHub

--- 백준/Gold/32944. 잘못된 LIS 알고리즘/잘못된 LIS 알고리즘.cc
+++ 백준/Gold/32944. 잘못된 LIS 알고리즘/잘못된 LIS 알고리즘.cc
@@ -3,13 +3,11 @@
 #define LL long long
 #include <iostream>
 #include <algorithm>
-#include <deque>
 #include <vector>
-
 using namespace std;
 
 int N, M, K;
-deque<int> dq;
+vector<int> dq;  // deque 대신 vector 사용 (cache locality 향상)
 vector<int> arr;
 
 int main() {
@@ -19,29 +17,43 @@ int main() {
 		printf("-1");
 	}
 	else {
+		// 메모리 재할당 최소화를 위한 reserve
+		arr.reserve(N);
+		dq.reserve(N - K);
+		
 		arr.resize(K);
 		rep(i, 1, K)
 			arr[i - 1] = i;
 		arr[K - 1] = N;
-
+		
 		rep(i, K, N)
 			dq.push_back(i);
-
+		
 		vector<int> tmp;
+		tmp.reserve(M - K + 1);  // 메모리 재할당 최소화
+		
 		rep(i, 0, M - K + 1) {
 			tmp.push_back(dq.back());
 			dq.pop_back();
 		}
+		
 		sort(tmp.begin(), tmp.end());
-		for (const int& x : tmp) {
-			arr.push_back(x);
-		}
-		while (dq.size()) {
+		
+		// 더 효율적인 삽입 (iterator 사용)
+		arr.insert(arr.end(), tmp.begin(), tmp.end());
+		
+		// 역순으로 남은 원소들 추가
+		while (!dq.empty()) {
 			arr.push_back(dq.back());
 			dq.pop_back();
 		}
-		for (const int& x : arr) {
-			printf("%d ", x);
+		
+		// 더 빠른 출력 (단일 문자열로 구성 후 출력)
+		for (size_t i = 0; i < arr.size(); ++i) {
+			printf("%d", arr[i]);
+			if (i < arr.size() - 1) printf(" ");
 		}
 	}
+	
+	return 0;
 }
