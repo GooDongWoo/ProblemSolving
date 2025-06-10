8d3e63e85a245c9289debd15f9110c5e48082cb7
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-06-10 14:06:54 +0900

    [Gold IV] Title: 이진수씨의 하루 일과, Time: 240 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Gold/17830. 이진수씨의 하루 일과/이진수씨의 하루 일과.cc
+++ 백준/Gold/17830. 이진수씨의 하루 일과/이진수씨의 하루 일과.cc
@@ -11,41 +11,55 @@ int T, N;
 string num;
 
 void F1(string& num, char target) {
-	rep(i, 0, num.size()) {
-		if (num[i] == '?') {
-			num[i] = target;
-		}
-	}
+    rep(i, 0, num.size()) {
+        if (num[i] == '?') {
+            num[i] = target;
+        }
+    }
 }
 
 int chck(string& num) {
-	int fidx = num.find("1");
-	if (fidx == -1) {
-		return 1;
-	}
-	string n1 = "";
-	rep(i, fidx, num.size()) {
-		n1 += num[i];
-	}
-	if (n1.find('1', 1) == -1) {
-		return N + n1.size() - 1;
-	}
-	else {
-		return N + n1.size();
-	}
+    // 첫 번째 1의 위치 찾기
+    int fidx = -1;
+    rep(i, 0, num.size()) {
+        if (num[i] == '1') {
+            fidx = i;
+            break;
+        }
+    }
+    
+    if (fidx == -1) {
+        return 1;
+    }
+    
+    int b_bits = num.size() - fidx;
+    
+    bool has_one_after = false;
+    rep(i, fidx + 1, num.size()) {
+        if (num[i] == '1') {
+            has_one_after = true;
+            break;
+        }
+    }
+    
+    if (has_one_after) {
+        return N + b_bits;
+    } else {
+        return N + b_bits - 1;
+    }
 }
 
 int main() {
-	fastio;
-	cin >> T;
-	rep(test_case, 1, T + 1) {
-		cin >> N >> num;
-		string min1(num), max1(num);
-		F1(min1, '0');
-		F1(max1, '1');
-		int cnt1 = chck(min1);
-		int cnt2 = chck(max1);
-		printf("%d %d\n", cnt2, cnt1);
-	}
-	return 0;
+    fastio;
+    cin >> T;
+    rep(test_case, 1, T + 1) {
+        cin >> N >> num;
+        string min1(num), max1(num);
+        F1(min1, '0');
+        F1(max1, '1');
+        int cnt1 = chck(min1);
+        int cnt2 = chck(max1);
+        printf("%d %d\n", cnt2, cnt1);
+    }
+    return 0;
 }
