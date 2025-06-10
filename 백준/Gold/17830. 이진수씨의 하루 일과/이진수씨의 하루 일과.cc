84c4c833e17816b482d0897243d281686a7f2848
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-06-10 14:01:39 +0900

    [Gold IV] Title: 이진수씨의 하루 일과, Time: 636 ms, Memory: 9004 KB -BaekjoonHub

---
+++ 백준/Gold/17830. 이진수씨의 하루 일과/이진수씨의 하루 일과.cc
@@ -0,0 +1,52 @@
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0)
+#define rep(i, a, b) for (int i=a;i<b;++i)
+#define LL long long
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int T, N;
+string num;
+
+void F1(string& num, string target) {
+	int idx = 0;
+	while ((idx = num.find('?', idx)) != -1)
+		num.replace(idx, 1, target);
+}
+
+int chck(string& num) {
+	int fidx = num.find("1");
+	if (fidx == -1) {
+		return 1;
+	}
+	string n1 = "";
+	rep(i, fidx, num.size()) {
+		n1 += num[i];
+	}
+
+	if (n1.find('1', 1) == -1) {
+		return num.size() + n1.size() - 1;
+	}
+	else {
+		return num.size() + n1.size();
+	}
+}
+
+int main() {
+	fastio;
+	cin >> T;
+	rep(test_case, 1, T + 1) {
+		cin >> N >> num;
+		string min1(num), max1(num);
+		F1(min1, "0");
+		F1(max1, "1");
+
+		int cnt1 = chck(min1);
+		int cnt2 = chck(max1);
+
+		printf("%d %d\n", cnt2, cnt1);
+	}
+}
