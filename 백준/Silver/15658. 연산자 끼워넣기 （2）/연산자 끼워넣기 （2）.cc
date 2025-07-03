d5f710a90e694964af8eb9d5f669860522a2b545
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-07-04 00:34:31 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.cc
@@ -0,0 +1,61 @@
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0)
+#define rep(i, a, b) for (int i=a;i<b;++i)
+#define fi first
+#define se second
+#define LL long long
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int N, arr[11], optrs[4], maxV = -1e9, minV = 1e9, ans;
+
+void bt(int idx) {
+	if (idx == N) {
+		maxV = max(maxV, ans);
+		minV = min(minV, ans);
+		return;
+	}
+	else {
+		rep(i, 0, 4) {
+			if (optrs[i] > 0) {
+				--optrs[i];
+				int bef = ans;
+				if (i == 0) {
+					ans += arr[idx];
+				}
+				else if (i == 1) {
+					ans -= arr[idx];
+				}
+				else if (i == 2) {
+					ans *= arr[idx];
+				}
+				else {
+					if (ans > 0) {
+						ans /= arr[idx];
+					}
+					else {
+						ans = -(abs(ans) / arr[idx]);
+					}
+				}
+				bt(idx + 1);
+				++optrs[i];
+				ans = bef;
+			}
+		}
+	}
+}
+int main() {
+	fastio;
+	cin >> N;
+	rep(i, 0, N) {
+		cin >> arr[i];
+	}
+	rep(i, 0, 4) {
+		cin >> optrs[i];
+	}
+	ans = arr[0];
+	bt(1);
+	printf("%d\n%d", maxV, minV);
+}
