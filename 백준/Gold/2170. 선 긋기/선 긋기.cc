0ec6dee293b5f0f57cb440777499a24b90fdb80c
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-07-14 20:37:15 +0900

    [Gold V] Title: 선 긋기, Time: 452 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -1,42 +1,45 @@
 #define rep(i,a,b) for(int i=a;i<b;++i)
-
 #include<iostream>
-#include<vector>
 #include<algorithm>
-
 using namespace std;
 
-int N, ans;
-vector<pair<int, int>> arr;
-
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin >> N;
-	rep(i, 0, N) {
-		int a, b;
-		cin >> a >> b;
-		arr.push_back({ a,0 });
-		arr.push_back({ b,1 });
-	}
-	sort(arr.begin(), arr.end());
-	int idx = 1, cnter = 1, bef = arr[0].first;
-	while (idx < 2*N) {
-		if (arr[idx].second == 0) {
-			cnter++;
-		}
-		else {
-			cnter--;
-			if (cnter == 0) {
-				ans += arr[idx].first - bef;
-				idx++;
-				if (idx < 2*N) {
-					bef = arr[idx].first;
-					cnter++;
-				}
-			}
-		}
-		idx++;
-	}
-	cout << ans;
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    
+    // 정적 배열 사용으로 메모리 할당 최적화
+    static pair<int, int> events[2000020];
+    
+    rep(i, 0, N) {
+        int x, y;
+        cin >> x >> y;
+        events[2*i] = {x, 0};     // 선분 시작점
+        events[2*i+1] = {y, 1};   // 선분 끝점
+    }
+    
+    sort(events, events + 2*N);
+    
+    long long ans = 0;  // 좌표 범위가 클 수 있으므로 long long 사용
+    int active_count = 0;
+    int union_start = 0;
+    
+    rep(i, 0, 2*N) {
+        if (events[i].second == 0) {  // 선분 시작
+            if (active_count == 0) {
+                union_start = events[i].first;
+            }
+            active_count++;
+        } else {  // 선분 끝
+            active_count--;
+            if (active_count == 0) {
+                ans += events[i].first - union_start;
+            }
+        }
+    }
+    
+    cout << ans << '\n';
+    return 0;
 }
