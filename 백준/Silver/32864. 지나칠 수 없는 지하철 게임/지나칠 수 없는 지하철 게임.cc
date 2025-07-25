fb441b1ab8d864f933eba227f933dcad261f2818
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-07-25 13:26:27 +0900

    [Silver I] Title: 지나칠 수 없는 지하철 게임, Time: 36 ms, Memory: 4644 KB -BaekjoonHub

--- 백준/Silver/32864. 지나칠 수 없는 지하철 게임/지나칠 수 없는 지하철 게임.cc
+++ 백준/Silver/32864. 지나칠 수 없는 지하철 게임/지나칠 수 없는 지하철 게임.cc
@@ -2,64 +2,63 @@
 #include <vector>
 #include <algorithm>
 
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
+
 using namespace std;
 
 int N;
-vector<int> A;
-vector<int> transfer_stations;
+bool arr[500000];
+vector<int> compressed;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(0);
-    cin >> N;
-    A.resize(N + 1);
-    for (int i = 1; i <= N; ++i) {
-        cin >> A[i];
-        if (i > 1 && i < N && A[i] == 1) {
-            transfer_stations.push_back(i);
-        }
-    }
-
-    if (N == 2) {
-        cout << "mnx" << endl;
-        return 0;
-    }
-
-    if (transfer_stations.empty()) {
-        cout << "mnx" << endl;
-        return 0;
-    }
-
-    vector<bool> is_winning_at_transfer(transfer_stations.size());
-
-    bool next_point_is_win = false;
-
-    for (int i = transfer_stations.size() - 1; i >= 0; --i) {
-        int current_station_idx = transfer_stations[i];
-        int next_station_idx = (i == transfer_stations.size() - 1) ? N : transfer_stations[i + 1];
-
-        int normal_stations_count = next_station_idx - current_station_idx - 1;
-
-        if (normal_stations_count == 0) {
-            is_winning_at_transfer[i] = !next_point_is_win;
-        }
-        else {
-            is_winning_at_transfer[i] = true;
-        }
-        next_point_is_win = is_winning_at_transfer[i];
-    }
-
-    int first_transfer_idx = transfer_stations[0];
-    if (first_transfer_idx > 2) {
-        cout << "mnx" << endl;
-    }
-    else {
-        if (is_winning_at_transfer[0]) {
-            cout << "alsdkffhgk" << endl; // 민아
-        }
-        else {
-            cout << "mnx" << endl; // 세훈
-        }
-    }
-
-    return 0;
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> N;
+	compressed.reserve(N);
+	rep(i, 0, N) {
+		cin >> arr[i];
+	}
+	if (N == 2) {
+		cout << "mnx";
+		return 0;
+	}
+	compressed.push_back(arr[1]);
+	rep(i, 2, N - 1) {
+		if (!(compressed.back() == 0 && arr[i] == 0)) {
+			compressed.push_back(arr[i]);
+		}
+	}
+	while (compressed.size() && compressed.back() == 0) {
+		compressed.pop_back();
+	}
+	N = compressed.size();
+	if (N == 0) {
+		cout << "mnx";
+		return 0;
+	}
+	vector<bool> dp(N);
+	for (int i = N - 2; i > -1; --i) {
+		if (i + 2 < N) {
+			if (compressed[i] == 0) {
+				dp[i] = !dp[i + 1];
+			}
+			else {
+				if (compressed[i + 1] == 1) {
+					dp[i] = !dp[i + 1];
+				}
+				else {
+					dp[i] = 0;
+				}
+			}
+		}
+		else {
+			dp[i] = 1;
+		}
+	}
+	if (dp[0] == 1 || (compressed[0] == 0 && dp[1] == 1)) {
+		cout << "mnx";
+	}
+	else {
+		cout << "alsdkffhgk";
+	}
+	return 0;
 }
