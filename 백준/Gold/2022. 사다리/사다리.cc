3e45169286478f23cf747797dc9f3a207a3f6a50
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-08-02 14:17:19 +0900

    [Gold IV] Title: 사다리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/2022. 사다리/사다리.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#define rep(i,a,b) for(LL i=(a);i<(b);++i)
+#define LL long long
+using namespace std;
+
+double a, b, c;
+
+double calc(double z) {
+	z = z * z;
+	return sqrt((a - z) * (b - z)) / (sqrt(b - z) + sqrt(a - z));
+}
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> a >> b >> c;
+	a = a * a;
+	b = b * b;
+	double start = 0, end = min(a, b);
+	while (end-start > 1e-6) {
+		double mid = mid = (start + end) / 2;
+		if (c < calc(mid)) {
+			start = mid;
+		}
+		else {
+			end = mid;
+		}
+	}
+	cout << start;
+	return 0;
+}
