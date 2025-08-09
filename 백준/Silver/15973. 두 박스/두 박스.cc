06b861dfcf2f6763f955dd54580521ca0cd40ac1
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-08-09 18:40:56 +0900

    [Silver I] Title: 두 박스, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/15973. 두 박스/두 박스.cc
@@ -0,0 +1,46 @@
+#define rep(i,a,b) for(int i=a;i<b;++i)
+#define LL long long
+#include <iostream>
+#include <algorithm>
+#include <tuple>
+#include <vector>
+using namespace std;
+
+struct rect {
+    int x1, y1, x2, y2;
+    bool operator< (const rect& b) const {
+        return tie(x1, y1) < tie(b.x1, b.y1);
+    }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int a, b, c, d;
+    rect r[2];
+    
+    rep(i, 0, 2) {
+        cin >> a >> b >> c >> d;
+        r[i] = { a, b, c, d };
+    }
+    sort(r, r + 2);
+    
+    if ((r[1].x1 > r[0].x2) || (r[1].y2 < r[0].y1) || (r[1].y1 > r[0].y2)) {
+        // 안겹침
+        cout << "NULL\n";
+    }
+    else if ((r[0].x2 == r[1].x1 && r[0].y1 == r[1].y2) || (r[0].x2 == r[1].x1 && r[0].y2 == r[1].y1)) {
+        // 점 교차
+        cout << "POINT\n";
+    }
+    else if ((r[0].x2 > r[1].x1 && r[0].y1 == r[1].y2) || (r[0].x2 > r[1].x1 && r[0].y2 == r[1].y1) 
+             || (r[0].x2 == r[1].x1)) {
+        // 선 교차
+        cout << "LINE\n";
+    }
+    else {
+        // 사각 교차
+        cout << "FACE\n";
+    }
+    
+    return 0;
+}
