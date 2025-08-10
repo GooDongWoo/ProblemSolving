cd8f48362f9a6292278fb8f48016a4ecf6647c80
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-08-10 17:57:57 +0900

    [Platinum IV] Title: 도로 네트워크, Time: 168 ms, Memory: 28388 KB -BaekjoonHub

--- 백준/Platinum/3176. 도로 네트워크/도로 네트워크.cc
+++ 백준/Platinum/3176. 도로 네트워크/도로 네트워크.cc
@@ -1,116 +1,117 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 #include <algorithm>
-#include <queue>
-#define rep(i,a,b) for(LL i=(a);i<(b);++i)
-#define LL long long
+#include <climits>
 using namespace std;
 
-int N, K, q1, q2, Rank[100000], min1, max1;
-vector<vector<pair<int, int>>> Adjv;
-int nDP[20][100000], minDP[20][100000], maxDP[20][100000];
-bool v[100000];
+const int MAXN = 100005;
+const int LOGN = 17; // log2(100000) + 1
 
-int LCA(int a, int b) {
-	if (Rank[a] != Rank[b]) {
-		if (Rank[a] > Rank[b]) {
-			swap(a, b);
-		}
-		int diff = Rank[b] - Rank[a];
-		while (diff > 0) {
-			int logVal = 19;
-			while ((1 << logVal) > diff) {
-				logVal--;
-			}
-			min1 = min(min1, minDP[logVal][b]);
-			max1 = max(max1, maxDP[logVal][b]);
-			b = nDP[logVal][b];
-			diff -= (1 << logVal);
-		}
-	}
-	while (a != b) {
-		int logVal = 0;
-		while (nDP[logVal][a] != nDP[logVal][b]) {
-			logVal++;
-		}
-		if (logVal != 0) {
-			min1 = min(min1, minDP[logVal - 1][a]);
-			max1 = max(max1, maxDP[logVal - 1][a]);
-			min1 = min(min1, minDP[logVal - 1][b]);
-			max1 = max(max1, maxDP[logVal - 1][b]);
+int N, K;
+vector<vector<pair<int, int>>> adj;
+int parent[LOGN][MAXN], minEdge[LOGN][MAXN], maxEdge[LOGN][MAXN];
+int depth[MAXN];
 
-			a = nDP[logVal - 1][a];
-			b = nDP[logVal - 1][b];
-		}
-		else {
-			min1 = min(min1, minDP[logVal][a]);
-			max1 = max(max1, maxDP[logVal][a]);
-			min1 = min(min1, minDP[logVal][b]);
-			max1 = max(max1, maxDP[logVal][b]);
-
-			a = nDP[logVal][a];
-			b = nDP[logVal][b];
-		}
-	}
-	return a;
+inline void dfs(int u, int p, int d) {
+    depth[u] = d;
+    parent[0][u] = p;
+    
+    for (auto& edge : adj[u]) {
+        int v = edge.first;
+        int w = edge.second;
+        if (v != p) {
+            minEdge[0][v] = maxEdge[0][v] = w;
+            dfs(v, u, d + 1);
+        }
+    }
 }
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin >> N;
-	Adjv.resize(N);
-	rep(i, 0, N - 1) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		a--; b--;
-		Adjv[a].push_back({ b,c });
-		Adjv[b].push_back({ a,c });
-	}
-	// bfs로 트리 생성
-	queue<int> q;
-	Rank[0] = 0;
-	v[0] = 1;
-	q.push(0);
-	while (q.size()) {
-		int cur = q.front(); q.pop();
-		for (const auto& nxtp : Adjv[cur]) {
-			int nxt = nxtp.first;
-			int w = nxtp.second;
-			if (v[nxt]) {
-				continue;
-			}
-			v[nxt] = 1;
-			Rank[nxt] = Rank[cur] + 1;
-			nDP[0][nxt] = cur;
-			minDP[0][nxt] = w;
-			maxDP[0][nxt] = w;
-			q.push(nxt);
-		}
-	}
-	//트리로 희소맵
-	//1. 해당 노드
-	//2. 해당 노드까지 중에서 최소 거리
-	//3. 해당 노드까지 중에서 최대 거리
-	rep(steps, 0, 20) {
-		minDP[steps][0] = 1e9;
-		maxDP[steps][0] = -1e9;
-	}
-	rep(steps, 1, 20) {
-		rep(Node, 1, N) {
-			nDP[steps][Node] = nDP[steps - 1][nDP[steps - 1][Node]];
-			minDP[steps][Node] = min(minDP[steps - 1][Node], minDP[steps - 1][nDP[steps - 1][Node]]);
-			maxDP[steps][Node] = max(maxDP[steps - 1][Node], maxDP[steps - 1][nDP[steps - 1][Node]]);
-		}
-	}
 
-	//쿼리 입력받고 로그로 끝내면서 쿼리 처리
-	cin >> K;
-	rep(i, 0, K) {
-		min1 = 1e9;
-		max1 = -1e9;
-		cin >> q1 >> q2;
-		q1--; q2--;
-		int lca = LCA(q1, q2);
-		cout << min1 << ' ' << max1 << '\n';
-	}
-	return 0;
+inline void preprocess() {
+    // Binary lifting 전처리 - 메모리 접근 패턴 최적화
+    for (int j = 1; j < LOGN; j++) {
+        for (int i = 0; i < N; i++) {
+            int p = parent[j-1][i];
+            if (p != -1) {
+                parent[j][i] = parent[j-1][p];
+                minEdge[j][i] = min(minEdge[j-1][i], minEdge[j-1][p]);
+                maxEdge[j][i] = max(maxEdge[j-1][i], maxEdge[j-1][p]);
+            }
+        }
+    }
+}
+
+inline pair<int, int> query(int u, int v) {
+    int minVal = INT_MAX, maxVal = INT_MIN;
+    
+    if (depth[u] < depth[v]) {
+        int temp = u; u = v; v = temp; // swap 최적화
+    }
+    
+    // u를 v와 같은 깊이로 맞추기 - 비트 연산 최적화
+    int diff = depth[u] - depth[v];
+    for (int i = 0; diff; i++, diff >>= 1) {
+        if (diff & 1) {
+            minVal = min(minVal, minEdge[i][u]);
+            maxVal = max(maxVal, maxEdge[i][u]);
+            u = parent[i][u];
+        }
+    }
+    
+    if (u == v) return {minVal, maxVal};
+    
+    // LCA 찾기 - 역순으로 최적화
+    for (int i = LOGN - 1; i >= 0; i--) {
+        int pu = parent[i][u], pv = parent[i][v];
+        if (pu != pv) {
+            int minU = minEdge[i][u], minV = minEdge[i][v];
+            int maxU = maxEdge[i][u], maxV = maxEdge[i][v];
+            
+            minVal = min(minVal, min(minU, minV));
+            maxVal = max(maxVal, max(maxU, maxV));
+            u = pu; v = pv;
+        }
+    }
+    
+    // 마지막 단계
+    minVal = min(minVal, min(minEdge[0][u], minEdge[0][v]));
+    maxVal = max(maxVal, max(maxEdge[0][u], maxEdge[0][v]));
+    
+    return {minVal, maxVal};
+}
+
+int main() {
+    scanf("%d", &N);
+    adj.resize(N);
+    
+    for (int i = 0; i < N - 1; i++) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        a--; b--;
+        adj[a].emplace_back(b, c);
+        adj[b].emplace_back(a, c);
+    }
+    
+    // 초기화 최적화
+    for (int i = 0; i < LOGN; i++) {
+        for (int j = 0; j < N; j++) {
+            parent[i][j] = -1;
+            minEdge[i][j] = INT_MAX;
+            maxEdge[i][j] = INT_MIN;
+        }
+    }
+    
+    dfs(0, -1, 0);
+    preprocess();
+    
+    scanf("%d", &K);
+    for (int i = 0; i < K; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        u--; v--;
+        
+        auto result = query(u, v);
+        printf("%d %d\n", result.first, result.second);
+    }
+    
+    return 0;
 }
