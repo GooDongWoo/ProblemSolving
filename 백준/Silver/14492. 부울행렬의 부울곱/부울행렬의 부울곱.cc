21a6fceac1133e2f6d20016ef782b81f6d882689
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-08-21 22:07:06 +0900

    [Silver V] Title: 부울행렬의 부울곱, Time: 8 ms, Memory: 2748 KB -BaekjoonHub

--- 백준/Silver/14492. 부울행렬의 부울곱/부울행렬의 부울곱.cc
+++ 백준/Silver/14492. 부울행렬의 부울곱/부울행렬의 부울곱.cc
@@ -7,31 +7,44 @@
 #define LL long long
 using namespace std;
 
-int N,arr1[300][300],arr2[300][300],ans;
+int N, arr1[300][300], arr2[300][300], ans;
+LL rows[300][5], cols[300][5]; // 5개의 64비트 청크로 분할
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin >> N;
-	rep(i, 0, N) {
-		rep(j, 0, N) {
-			cin >> arr1[i][j];
-		}
-	}
-	rep(i, 0, N) {
-		rep(j, 0, N) {
-			cin >> arr2[i][j];
-		}
-	}
-	rep(i, 0, N) {
-		rep(j, 0, N) {
-			rep(k, 0, N) {
-				if (arr1[i][k] & arr2[k][j]) {
-					ans++;
-					break;
-				}
-			}
-		}
-	}
-	cout << ans;
-	return 0;
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> N;
+    
+    rep(i, 0, N) {
+        rep(j, 0, N) {
+            cin >> arr1[i][j];
+            if (arr1[i][j]) {
+                rows[i][j/64] |= (1LL << (j%64));
+            }
+        }
+    }
+    
+    rep(i, 0, N) {
+        rep(j, 0, N) {
+            cin >> arr2[i][j];
+            if (arr2[i][j]) {
+                cols[j][i/64] |= (1LL << (i%64));
+            }
+        }
+    }
+    
+    rep(i, 0, N) {
+        rep(j, 0, N) {
+            bool found = false;
+            rep(chunk, 0, 5) {
+                if (rows[i][chunk] & cols[j][chunk]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) ans++;
+        }
+    }
+    
+    cout << ans;
+    return 0;
 }
