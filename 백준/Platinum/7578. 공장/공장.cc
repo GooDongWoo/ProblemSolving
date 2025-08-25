2a36e9e7e7cd40f8fae53db80602478568735b2f
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-08-25 21:55:52 +0900

    [Platinum V] Title: 공장, Time: 128 ms, Memory: 7880 KB -BaekjoonHub

--- 백준/Platinum/7578. 공장/공장.cc
+++ 백준/Platinum/7578. 공장/공장.cc
@@ -1,22 +1,21 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 #define rep(i,a,b) for(int i=(a);i<(b);++i)
 #define LL long long
 using namespace std;
 
-LL N, arr[500001], ans, dat[1000001];
+int N, arr[500001], dat[1000001];
+LL ans;
 
-void update(LL idx, LL val) {
+void update(int idx, int val) {
 	while (idx <= N) {
 		arr[idx] += val;
 		idx += idx & (~idx + 1);
 	}
 }
 
-LL query(LL idx) {
-	LL cur = 0;
+int query(int idx) {
+	int cur = 0;
 	while (idx > 0) {
 		cur += arr[idx];
 		idx -= idx & (~idx + 1);
@@ -29,14 +28,14 @@ int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin >> N;
 	rep(i, 1, N + 1) {
-		LL a;
+		int a;
 		cin >> a;
 		dat[a] = i;
 	}
 	rep(i, 0, N) {
-		LL a;
+		int a;
 		cin >> a;
-		ans += (i - query(dat[a]));
+		ans += (LL)(i - query(dat[a]));
 		update(dat[a], 1);
 	}
 	cout << ans;
