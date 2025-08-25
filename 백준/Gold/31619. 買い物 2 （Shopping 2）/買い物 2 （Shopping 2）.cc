03eddb91d1defdcc58b819c2c9b374cee56a40e8
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-08-25 22:44:59 +0900

    [Gold I] Title: 買い物 2 (Shopping 2), Time: 368 ms, Memory: 31604 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31619. 買い物 2 （Shopping 2）/買い物 2 （Shopping 2）.cc
+++ 백준/Gold/31619. 買い物 2 （Shopping 2）/買い物 2 （Shopping 2）.cc
@@ -1,76 +1,97 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <tuple>
+
+using ll = long long;
+
+std::vector<ll> seg_tree;
+int N; 
+
+void update(int node, int start, int end, int idx, int value) {
+    if (start == end) {
+        seg_tree[node] += value;
+        return;
+    }
+    int mid = (start + end) / 2;
+    if (start <= idx && idx <= mid) {
+        update(2 * node, start, mid, idx, value);
+    } else {
+        update(2 * node + 1, mid + 1, end, idx, value);
+    }
+    seg_tree[node] = seg_tree[2 * node] + seg_tree[2 * node + 1];
+}
+
+ll query(int node, int start, int end, int l, int r) {
+    if (r < start || end < l) {
+        return 0; 
+    }
+    if (l <= start && end <= r) {
+        return seg_tree[node]; 
+    }
+    int mid = (start + end) / 2;
+    ll p1 = query(2 * node, start, mid, l, r);
+    ll p2 = query(2 * node + 1, mid + 1, end, l, r);
+    return p1 + p2;
+}
+
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int N, M, Q;
-    cin >> N >> M >> Q;
-    
-    vector<pair<ll, int>> products(N);
-    vector<vector<int>> type_indices(M + 1);
-    
-    bool all_price_2 = true;
-    
-    for(int i = 0; i < N; i++) {
-        cin >> products[i].first >> products[i].second;
-        if(products[i].first != 2) all_price_2 = false;
-        type_indices[products[i].second].push_back(i);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int M, Q;
+    std::cin >> N >> M >> Q;
+
+    std::vector<std::vector<std::pair<int, int>>> products_by_type(M + 1);
+    std::vector<ll> total_prefix_sum(N + 1, 0);
+
+    for (int i = 1; i <= N; ++i) {
+        int p, a;
+        std::cin >> p >> a;
+        products_by_type[a].push_back({i, p});
+        total_prefix_sum[i] = total_prefix_sum[i - 1] + p;
     }
-    
-    bool all_types_different = true;
-    for(int i = 1; i <= M; i++) {
-        if(type_indices[i].size() > 1) {
-            all_types_different = false;
-            break;
-        }
+
+    std::vector<std::vector<std::tuple<int, int, int>>> queries_by_day(M + 1);
+    for (int i = 0; i < Q; ++i) {
+        int t, l, r;
+        std::cin >> t >> l >> r;
+        queries_by_day[t].push_back({l, r, i});
     }
+
+    seg_tree.resize(4 * N + 4, 0);
     
-    vector<ll> prefix_sum(N + 1, 0);
-    for(int i = 0; i < N; i++) {
-        prefix_sum[i + 1] = prefix_sum[i] + products[i].first;
-    }
-    
-    while(Q--) {
-        int day, L, R;
-        cin >> day >> L >> R;
-        L--;
-        
-        ll total = prefix_sum[R] - prefix_sum[L];
-        ll discount = 0;
-        
-        if(M == 1) {
-            if(day == 1) {
-                cout << total / 2 << '\n';
-                continue;
-            }
-            cout << total << '\n';
-            continue;
+    std::vector<ll> answers(Q);
+
+    for (int day = 1; day <= M; ++day) {
+        for (const auto& product : products_by_type[day]) {
+            int product_idx = product.first;
+            int product_price = product.second;
+            update(1, 1, N, product_idx, product_price / 2);
         }
-        
-        if(all_price_2) {
-            int count = 0;
-            for(int i = L; i < R; i++) {
-                if(products[i].second == day) count++;
-            }
-            cout << 2 * (R - L) - count << '\n';
-            continue;
-        }
-        
-        if(!type_indices[day].empty()) {
-            auto start = lower_bound(type_indices[day].begin(), type_indices[day].end(), L);
-            auto end = upper_bound(type_indices[day].begin(), type_indices[day].end(), R-1);
+
+        for (const auto& q : queries_by_day[day]) {
+            int l, r, original_idx;
+            std::tie(l, r, original_idx) = q;
+
+            ll full_price_sum = total_prefix_sum[r] - total_prefix_sum[l - 1];
+
+            ll discount_sum = query(1, 1, N, l, r);
             
-            for(auto it = start; it != end; ++it) {
-                int idx = *it;
-                discount += products[idx].first / 2;
-            }
+            answers[original_idx] = full_price_sum - discount_sum;
         }
         
-        cout << total - discount << '\n';
+        for (const auto& product : products_by_type[day]) {
+            int product_idx = product.first;
+            int product_price = product.second;
+            update(1, 1, N, product_idx, -product_price / 2);
+        }
     }
-    
+
+    for (int i = 0; i < Q; ++i) {
+        std::cout << answers[i] << "\n";
+    }
+
     return 0;
 }
