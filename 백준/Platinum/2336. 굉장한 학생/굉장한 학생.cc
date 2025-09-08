2513cd4b3b6e1e4b323bacad2cd26c44d0730c4f
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-09-08 23:10:45 +0900

    [Platinum II] Title: 굉장한 학생, Time: 304 ms, Memory: 15700 KB -BaekjoonHub

--- 백준/Platinum/2336. 굉장한 학생/굉장한 학생.cc
+++ 백준/Platinum/2336. 굉장한 학생/굉장한 학생.cc
@@ -1,74 +1,80 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <cstring>
+#include <algorithm>
 
-#define rep(i,a,b) for(int i=(a);i<(b);++i)
-
-using namespace std;
+const int INF = 1e9;
 
 struct Student {
-	int id;
-	int ranks[3];
-	bool operator<(const Student& other) const {
-		return ranks[0] < other.ranks[0];
-	}
+    int r0, r1, r2;
+
+    bool operator<(const Student& other) const {
+        return r0 < other.r0;
+    }
 };
 
-int N, segtree[2000004], ans, temp_rank[500001][3];
-Student students[500001];
+int N;
+std::vector<int> segTree;
+std::vector<Student> students;
 
-void update(int idx, int l, int r, int qidx, int val) {
-	if (qidx < l || r < qidx) return;
-	if (l == r) {
-		segtree[idx] = min(segtree[idx], val); 
-		return;
-	}
-	int mid = (l + r) / 2;
-	update(2 * idx, l, mid, qidx, val);
-	update(2 * idx + 1, mid + 1, r, qidx, val);
-	segtree[idx] = min(segtree[2 * idx], segtree[2 * idx + 1]);
+void update(int node, int start, int end, int qIdx, int val) {
+    if (qIdx < start || end < qIdx) return;
+    if (start == end) {
+        segTree[node] = std::min(segTree[node], val);
+        return;
+    }
+    int mid = (start + end) / 2;
+    update(2 * node, start, mid, qIdx, val);
+    update(2 * node + 1, mid + 1, end, qIdx, val);
+    segTree[node] = std::min(segTree[2 * node], segTree[2 * node + 1]);
 }
 
-int query(int idx, int l, int r, int ql, int qr) {
-	if (qr < l || r < ql || ql > qr) return 1e9;
-	if (ql <= l && r <= qr) return segtree[idx];
-	int mid = (l + r) / 2;
-	return min(query(2 * idx, l, mid, ql, qr), query(2 * idx + 1, mid + 1, r, ql, qr));
+int query(int node, int start, int end, int qStart, int qEnd) {
+    if (qEnd < start || end < qStart || qStart > qEnd) return INF;
+    if (qStart <= start && end <= qEnd) return segTree[node];
+    int mid = (start + end) / 2;
+    return std::min(query(2 * node, start, mid, qStart, qEnd), 
+                    query(2 * node + 1, mid + 1, end, qStart, qEnd));
 }
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin >> N;
-	rep(i, 1, 4 * (N + 1)) {
-		segtree[i] = 1e9;
-	}
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
 
-	rep(i, 0, 3) {
-		rep(j, 1, N + 1) {
-			int student_id;
-			cin >> student_id;
-			temp_rank[student_id][i] = j;
-		}
-	}
+    std::cin >> N;
 
-	rep(i, 1, N + 1) {
-		students[i].id = i;
-		students[i].ranks[0] = temp_rank[i][0];
-		students[i].ranks[1] = temp_rank[i][1];
-		students[i].ranks[2] = temp_rank[i][2];
-	}
+    students.resize(N);
+    for (int i = 0; i < N; ++i) {
+        int id;
+        std::cin >> id;
+        students[id - 1].r0 = i + 1;
+    }
+    for (int i = 0; i < N; ++i) {
+        int id;
+        std::cin >> id;
+        students[id - 1].r1 = i + 1;
+    }
+    for (int i = 0; i < N; ++i) {
+        int id;
+        std::cin >> id;
+        students[id - 1].r2 = i + 1;
+    }
+    
+    std::sort(students.begin(), students.end());
 
-	sort(students + 1, students + N + 1);
-	rep(i, 1, N + 1) {
-		update(1, 1, N, students[i].ranks[1], students[i].ranks[2]);
-		if (students[i].ranks[1] == 1) {
-			ans++;
-			continue;
-		}
-		ans += query(1, 1, N, 1, students[i].ranks[1] - 1) < students[i].ranks[2] ? 0 : 1;
-	}
-	cout << ans;
-	return 0;
+    segTree.assign(4 * N, INF);
+    
+    int nonInferiorCount = 0;
+    for (int i = 0; i < N; ++i) {
+        int minR2 = query(1, 1, N, 1, students[i].r1 - 1);
+        
+        if (minR2 > students[i].r2) {
+            nonInferiorCount++;
+        }
+        
+        update(1, 1, N, students[i].r1, students[i].r2);
+    }
 
+    std::cout << nonInferiorCount;
+
+    return 0;
 }
