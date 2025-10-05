f92493e27c34f35f291434b10baaaf3dd01cec72
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-10-05 13:02:23 +0900

    [Gold IV] Title: 전화번호 목록, Time: 104 ms, Memory: 7580 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -1,77 +1,98 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <string>
+#include <vector>
 
-#define rep(i,a,b) for(int i=a;i<b;++i)
-using LL = long long;
+#define rep(i, a, b) for (int i = a; i < b; ++i)
 
 using namespace std;
 
-
-int T, N;
-bool Flag;
-
 struct Node {
-	bool IsEnd;
-	Node* child[10];
-	Node() :IsEnd(false) {
-		rep(i, 0, 10) {
-			child[i] = nullptr;
-		}
-	}
-};
-struct Trie {
-	Node head;
-	Trie() {
-	}
-	void insert(string& word, Node* cur, int idx) {
-		Node* nxtp;
-		if (cur->child[word[idx] - '0']) {
-			nxtp = cur->child[word[idx] - '0'];
-		}
-		else {
-			nxtp = new Node();
-			cur->child[word[idx] - '0'] = nxtp;
-		}
-		if (nxtp->IsEnd) {
-			Flag = 0;
-		}
-		if (idx == word.size() - 1) {
-			nxtp->IsEnd = true;
-			int tFlag = 1;
-			rep(i, 0, 10) {
-				if (nxtp->child[i]) {
-					Flag = 0;
-				}
-			}
-		}
-		else {
-			insert(word, nxtp, idx + 1);
-		}
-	}
+    bool IsEnd;
+    Node* child[10];
+    Node() : IsEnd(false) {
+        rep(i, 0, 10) {
+            child[i] = nullptr;
+        }
+    }
 };
 
+class Trie {
+private:
+    Node* root;
+
+    void clear(Node* node) {
+        if (node == nullptr) return;
+        rep(i, 0, 10) {
+            clear(node->child[i]);
+        }
+        delete node;
+    }
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    ~Trie() {
+        clear(root);
+    }
+
+    bool insert(const string& word) {
+        Node* current = root;
+        for (char ch : word) {
+            int num = ch - '0';
+            if (current->child[num] == nullptr) {
+                current->child[num] = new Node();
+            }
+            current = current->child[num];
+
+            if (current->IsEnd) {
+                return false;
+            }
+        }
+
+        rep(i, 0, 10) {
+            if (current->child[i] != nullptr) {
+                return false;
+            }
+        }
+
+        current->IsEnd = true;
+        return true;
+    }
+};
+
+void solve() {
+    int N;
+    cin >> N;
+    Trie trie;
+    bool isConsistent = true;
+    vector<string> numbers(N);
+
+    rep(i, 0, N) {
+        cin >> numbers[i];
+    }
+
+    rep(i, 0, N) {
+        if (isConsistent && !trie.insert(numbers[i])) {
+            isConsistent = false;
+        }
+    }
+
+    if (isConsistent) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
 
 int main() {
-	cin.tie(0)->sync_with_stdio(false);
-	cout.tie(0);
-	cin >> T;
-	rep(test_case, 1, T + 1) {
-		cin >> N;
-		Trie trie = Trie();
-		Flag = 1;
-		rep(i, 0, N) {
-			string word;
-			cin >> word;
-			trie.insert(word, &trie.head, 0);
-		}
-		if (Flag) {
-			cout << "YES\n";
-		}
-		else {
-			cout << "NO\n";
-		}
-	}
-	return 0;
+    cin.tie(0)->sync_with_stdio(false);
+    cout.tie(0);
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
 }
