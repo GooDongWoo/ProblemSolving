57ceac5f4b5955ce4669e96ee30fee7b4b4f2594
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-10-10 20:50:00 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 36 ms, Memory: 4364 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+using LL = long long;
+using namespace std;
+
+int N, mat[500][500], dp[500][500], ans;
+bool v[500][500];
+int dys[] = { 1, 0, -1, 0 };
+int dxs[] = { 0, 1, 0, -1 };
+
+int dfs(int cy, int cx, int bef)
+{
+    dp[cy][cx] = 1;
+    rep(step, 0, 4)
+    {
+        int ny = cy + dys[step];
+        int nx = cx + dxs[step];
+        if (!(0 <= ny && ny < N && 0 <= nx && nx < N) || mat[ny][nx] <= mat[cy][cx])
+            continue;
+        if (dp[ny][nx] == 0)
+            dp[cy][cx] = max(dp[cy][cx], dfs(ny, nx, bef + 1) + 1);
+        else
+            dp[cy][cx] = max(dp[cy][cx], dp[ny][nx] + 1);
+    }
+    return dp[cy][cx];
+}
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    cout.tie(0);
+    cin >> N;
+    rep(i, 0, N)
+    {
+        rep(j, 0, N)
+        {
+            cin >> mat[i][j];
+        }
+    }
+    rep(i, 0, N)
+    {
+        rep(j, 0, N)
+        {
+            if (dp[i][j])
+            {
+                continue;
+            }
+            ans = max(ans, dfs(i, j, 0));
+        }
+    }
+    cout << ans;
+}
