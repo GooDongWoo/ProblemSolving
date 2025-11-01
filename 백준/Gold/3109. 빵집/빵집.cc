0d7e5dcb040e6d61897a2d628b14d5639c5cac74
DongWoo-Goo <59087923+GooDongWoo@users.noreply.github.com>
2025-11-01 13:09:54 +0900

    [Gold II] Title: 빵집, Time: 96 ms, Memory: 11788 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstring>
+
+#define rep(i,a,b) for(int i=a;i<b;++i)
+using LL = long long;
+
+using namespace std;
+
+int R, C, ans;
+char mat[10000][500];
+bool v[10000][500];
+int drs[] = { 1,0,-1 };
+int dcs[] = { 1,1,1 };
+
+bool dfs(int r, int c) {
+	v[r][c] = true;
+	if (c == C - 1) {
+		ans++;
+		return true;
+	}
+	rep(step, 0, 3) {
+		int nr = r + drs[step];
+		int nc = c + dcs[step];
+		if (!(0 <= nr && nr < R && 0 <= nc && nc < C) || mat[nr][nc] == 'x' || v[nr][nc]) {
+			continue;
+		}
+		bool ret = dfs(nr, nc);
+		if (ret) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cout.tie(0);
+	cin >> R >> C;
+	rep(i, 0, R) {
+		rep(j, 0, C) {
+			cin >> mat[i][j];
+		}
+	}
+	rep(i, 0, R) {
+		int startRow = R - 1 - i;
+		dfs(startRow, 0);
+	}
+	cout << ans;
+
+	return 0;
+}
