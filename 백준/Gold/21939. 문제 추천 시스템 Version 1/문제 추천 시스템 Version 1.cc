95648c06729a23818916c396ce15971331b1c56b
GooDongWoo <59087923+GooDongWoo@users.noreply.github.com>
2026-02-08 15:18:18 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 20 ms, Memory: 5236 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.cc
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.cc
@@ -1,89 +1,64 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
-#include <cmath>
-#include <map>
-#include <unordered_map>
-#include <cmath>
 #include <string>
-#include <cstring>
-#include <stdexcept>
-
-#define rep(i,a,b) for(int (i)=(a);i<(b);++(i))
 
 using namespace std;
-using LL = long long;
-using UI = unsigned int;
 
-int N, M, ans[100001];
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minq;
-priority_queue<pair<int, int>> maxq;
+const int MAX_P = 100002;
 
+int ans[MAX_P];
+priority_queue<pair<int, int>> maxq; 
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minq; // 오름차순
 
 int main() {
-	cin.tie(NULL)->sync_with_stdio(false);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	cin >> N;
-	rep(i, 0, N) {
-		int p, l;
-		cin >> p >> l;
-		ans[p] = l;
-		minq.push({ l,p });
-		maxq.push({ l,p });
-	}
-	cin >> M;
-	rep(i, 0, M) {
-		string cmd;
-		cin >> cmd;
-		if (cmd == "add") {
-			int p, l;
-			cin >> p >> l;
-			ans[p] = l;
-			minq.push({ l,p });
-			maxq.push({ l,p });
-		}
-		else if (cmd == "solved") {
-			int p;
-			cin >> p;
-			ans[p] = 0;
-		}
-		else {
-			int what;
-			cin >> what;
-			if (what == 1) {
-				while (true) {
-					if (maxq.size()) {
-						if (ans[maxq.top().second] == maxq.top().first) {
-							cout << maxq.top().second<<'\n';
-							break;
-						}
-						else {
-							maxq.pop();
-						}
-					}
-					else {
-						throw runtime_error(" wrong scenario: empty maxq");
-					}
-				}
-			}
-			else {
-				while (true) {
-					if (minq.size()) {
-						if (ans[minq.top().second] == minq.top().first) {
-							cout << minq.top().second << '\n';
-							break;
-						}
-						else {
-							minq.pop();
-						}
-					}
-					else {
-						throw runtime_error(" wrong scenario: empty minq");
-					}
-				}
-			}
-		}
-	}
-	return 0;
+    int N;
+    cin >> N;
+    while(N--) {
+        int p, l;
+        cin >> p >> l;
+        ans[p] = l;
+        maxq.push({l, p});
+        minq.push({l, p});
+    }
+
+    int M;
+    cin >> M;
+    while(M--) {
+        string cmd;
+        cin >> cmd;
+
+        if (cmd[0] == 'a') { // add
+            int p, l;
+            cin >> p >> l;
+            ans[p] = l;
+            maxq.push({l, p});
+            minq.push({l, p});
+        }
+        else if (cmd[0] == 's') { 
+            int p;
+            cin >> p;
+            ans[p] = 0; 
+        }
+        else { 
+            int x;
+            cin >> x;
+            if (x == 1) {
+                while (!maxq.empty() && ans[maxq.top().second] != maxq.top().first) {
+                    maxq.pop();
+                }
+                cout << maxq.top().second << '\n';
+            }
+            else {
+                while (!minq.empty() && ans[minq.top().second] != minq.top().first) {
+                    minq.pop();
+                }
+                cout << minq.top().second << '\n';
+            }
+        }
+    }
+    return 0;
 }
